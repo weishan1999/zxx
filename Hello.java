dd171de51db53c75b0b91a0b94f32ccbf1aec56e
zxx <1198830423@qq.com>
2019-01-26 17:39:09 +0800

    Change Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello{
       public static void main(String args[]){
       	system.out.println("Hello world");
       	system.out.println("不要脸");
+      	system.out.println("不要脸");
       }
 
 
